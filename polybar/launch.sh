39deb9636fec49bc75df034a10b35d5115290aa7
Vague Penguin <vaguepenguindock@gmail.com>
2021-08-11 21:06:55 -0500

    updated polybar config to work with multiple monitors

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env sh
 
+PPATH=/home/e/.config/polybar
+
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-#while pgrep -x polybar >/dev/null; do sleep 1; done
+while pgrep -x polybar >/dev/null; do sleep 1; done
 
-cat "/home/e/.config/polybar/config" | sed 's/<<mpd-password>>/castle/g' > /home/e/.config/polybar/configp
+sh "$PPATH/preprocess.sh"
 
-# Launch
-polybar page --config="/home/e/.config/polybar/config" &
-polybar music --config="/home/e/.config/polybar/configp" &
-polybar songname --config="/home/e/.config/polybar/config" &
-polybar center --config="/home/e/.config/polybar/config" &
-polybar currenttool --config="/home/e/.config/polybar/config" &
-polybar system --config="/home/e/.config/polybar/config" &
+cat "$PPATH/config.live" | sed 's/<<mpd-password>>/castle/g' > "$PPATH/config.live.p"
+
+# Launch on all monitors
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+	polybar page-$m        --config="$PPATH/config.live" &
+	polybar music-$m       --config="$PPATH/config.live.p" &
+	polybar songname-$m    --config="$PPATH/config.live" &
+	polybar center-$m      --config="$PPATH/config.live" &
+	polybar currenttool-$m --config="$PPATH/config.live" &
+	polybar system-$m      --config="$PPATH/config.live"
+done
 
 echo "Bar launched..."
---
+++ polybar/preprocess.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+PPATH=/home/e/.config/polybar
+
+cat "$PPATH/shared/config.default" > "$PPATH/config.pre"
+
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+	for bar in $(ls "$PPATH/bars"); do
+		echo -en "\n" >> "$PPATH/config.pre"
+		cat "$PPATH/bars/$bar" | sed "s/#monitor/$m/g" | sed -E "s+(\[bar/.*-)+\1$m+" >> "$PPATH/config.pre"
+		echo -en "\n" >> "$PPATH/config.pre"
+		cat "$PPATH/shared/bars.shared" >> "$PPATH/config.pre"
+	done
+done
+
+# for mod in $(ls "$PPATH/modules"); do
+#	echo -en "\n" >> "$PPATH/config.pre"
+#       	cat "$PPATH/modules/$mod" >> "$PPATH/config.pre"
+# done
+
+cat "$PPATH/shared/modules" >> "$PPATH/config.pre"
+
+cat "$PPATH/config.pre" > "$PPATH/config.live"
+rm "$PPATH/config.pre"
