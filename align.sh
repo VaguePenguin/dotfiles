1149374f41863dbba75fa65f3072e811efe324b3
Vague Penguin <vaguepenguin@gmail.com>
2022-02-20 12:09:29 -0500

    .

---
+++ align.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+ln -s alacritty ~/.config/alacritty
+ln -s autorandr ~/.config/autorandr
+ln -s i3 ~/.config/i3
+ln -s mpd ~/.config/mpd
+ln -s picom.conf ~/.config/picom.conf
+ln -s polybar ~/.config/polybar
+
+ln -s efont ~/.fonts/efont
+
